e868b729bb50d129931b085baa889a8671ae26e8
Core2931 <mark.2631@hotmail.com>
2021-05-17 13:34:33 +0700

    test

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.CORS ?? "http://localhost:3000",
+    origin: process.env.CORS ?? "http://34.228.189.54/",
     credentials: true,
   })
 );
@@ -57,7 +57,7 @@ server.applyMiddleware({
   app,
   path,
   cors: {
-    origin: process.env.CORS ?? "http://localhost:3000",
+    origin: process.env.CORS ?? "http://34.228.189.54/",
     credentials: true,
   },
 });
